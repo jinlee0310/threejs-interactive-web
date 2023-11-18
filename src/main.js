c77602abcc62734cef6c8e0e25f306be55e218f3
jinlee0310 <onenmore.jin@gmail.com>
2023-11-18 22:04:49 +0900

    feat: setting initial threejs

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ window.addEventListener("load", () => {
     init();
 });
 
-function init() {}
+function init() {
+    const renderer = new Three.WebGLRenderer({
+        // alpha: true,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const scene = new Three.Scene();
+
+    const camera = new Three.PerspectiveCamera(
+        75, // 시야각
+        window.innerWidth / window.innerHeight, // 종횡비
+        1, // near
+        500 // far
+    );
+
+    renderer.render(scene, camera);
+}
