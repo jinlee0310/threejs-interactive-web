b3ad7720a4563c53a79d965242d8cda2477b8421
jinlee0310 <onenmore.jin@gmail.com>
2023-11-19 01:28:57 +0900

    feat: routing 추가

--- src/main.js
+++ src/main.js
@@ -1,111 +1,37 @@
-import * as Three from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import GUI from "lil-gui";
+import { renderCube } from "./container/Cube";
 
 window.addEventListener("load", () => {
     init();
 });
 
-function init() {
-    const options = {
-        color: 0x00ffff,
-    };
+const init = () => {
+    const { pathname } = window.location;
 
-    const renderer = new Three.WebGLRenderer({
-        // alpha: true,
-        antialias: true,
-    });
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
-
-    const scene = new Three.Scene();
-
-    const camera = new Three.PerspectiveCamera(
-        75, // 시야각
-        window.innerWidth / window.innerHeight, // 종횡비
-        1, // near
-        500 // far
-    );
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    controls.autoRotate = true;
-    // controls.autoRotateSpeed = 30;
-    controls.enableDamping = true;
-    // controls.dampingFactor = 0.01;
-    controls.enableZoom = true;
-    controls.enablePan = true;
-    // controls.maxDistance = 50;
-    // controls.minDistance = 10;
-    // controls.maxPolarAngle = Math.PI / 2;
-    // controls.minPolarAngle = Math.PI / 3;
-
-    const cubeGeometry = new Three.IcosahedronGeometry(1);
-    const cubeMaterial = new Three.MeshLambertMaterial({
-        color: 0x00ffff,
-        emissive: 0x111111,
-    });
-    const cube = new Three.Mesh(cubeGeometry, cubeMaterial);
-
-    const skeletonGeometry = new Three.IcosahedronGeometry(2);
-    const skeletonMaterial = new Three.MeshBasicMaterial({
-        wireframe: true,
-        transparent: true,
-        opacity: 0.2,
-        color: 0xaaaaaa,
-    });
-    const skeleton = new Three.Mesh(skeletonGeometry, skeletonMaterial);
-
-    scene.add(cube, skeleton);
-
-    camera.position.z = 5;
-
-    const directionalLight = new Three.DirectionalLight(0xffffff, 1);
-
-    scene.add(directionalLight);
-
-    const clock = new Three.Clock();
-
-    render();
-
-    function render() {
-        const elapsedTime = clock.getElapsedTime();
-
-        // cube.rotation.x = elapsedTime;
-        // cube.rotation.y = elapsedTime;
-
-        // skeleton.rotation.x = elapsedTime * 1.5;
-        // skeleton.rotation.y = elapsedTime * 1.5;
-
-        renderer.render(scene, camera);
-
-        controls.update();
-
-        requestAnimationFrame(render);
+    switch (pathname) {
+        case "/cube":
+            renderCube();
+            break;
+        case "/":
+        default:
+            renderNav();
+            break;
     }
+};
 
-    function handleResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
+const renderNav = () => {
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
 
-        camera.updateProjectionMatrix();
+    const menuList = ["cube"];
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
-        renderer.render(scene, camera);
-
-        controls.update();
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    const gui = new GUI();
-
-    gui.add(cube.position, "y", -3, 3, 1);
-    gui.add(cube, "visible");
-
-    gui.addColor(options, "color").onChange((v) => {
-        cube.material.color.set(v);
+    menuList.forEach((menu) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.setAttribute("href", `/${menu}`);
+        a.innerText = menu;
+        li.appendChild(a);
+        ul.appendChild(li);
     });
-}
+    nav.appendChild(ul);
+    document.body.appendChild(nav);
+};
