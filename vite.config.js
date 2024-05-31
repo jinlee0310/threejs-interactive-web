348f99767a9587a1c32022676574f6732f2c2166
jinlee0310 <onenmore.jin@gmail.com>
2024-05-31 17:11:06 +0900

    chore: vite-plugin-glsl 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-export default {
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
+
+export default defineConfig({
     root: "src",
     build: {
         outDir: "../dist",
         emptyOutDir: true,
     },
-};
+    plugins: [glsl()],
+});
