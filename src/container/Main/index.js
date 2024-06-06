8ec616f55e2d46d9ffd677a7c0cd4b29963cf301
jinlee0310 <onenmore.jin@gmail.com>
2024-06-07 08:02:55 +0900

    refactor: renderMain 함수 리팩토링

--- src/container/Main/index.js
+++ src/container/Main/index.js
@@ -28,69 +28,102 @@ export default function renderMain() {
 
     camera.position.set(0, 0, 50);
 
-    const ringGeometry = new Three.TorusGeometry(8, 1, 16, 100);
-    const ringMaterial = new Three.MeshStandardMaterial({ color: "#E53935" });
-    const ring = new Three.Mesh(ringGeometry, ringMaterial);
-
-    ring.position.set(30, 10, 0);
-
-    scene.add(ring);
-
-    const triangleGeometry = new Three.TorusGeometry(8, 1, 16, 3);
-    const triangleMaterial = new Three.MeshStandardMaterial({
-        color: "#4CAF50",
-    });
-    const triangle = new Three.Mesh(triangleGeometry, triangleMaterial);
-
-    triangle.position.set(-20, 20, 0);
-
-    scene.add(triangle);
-
-    const geometry = new Three.BoxGeometry(16, 16, 1);
-    const material = new Three.MeshStandardMaterial({ color: "#0e0398" });
-    const cube = new Three.Mesh(geometry, material);
-
-    cube.position.set(20, -20, 0);
-
-    scene.add(cube);
-
-    const ambientLight = new Three.AmbientLight(0xffffff, 0.8);
-
-    ambientLight.position.set(-5, -5, -5);
-
-    scene.add(ambientLight);
-
-    const axesHelper = new Three.AxesHelper(300);
-
     const controls = new OrbitControls(camera, renderer.domElement);
 
-    // scene.add(axesHelper);
+    const createRing = () => {
+        const ringGeometry = new Three.TorusGeometry(8, 1, 16, 100);
+        const ringMaterial = new Three.MeshStandardMaterial({
+            color: "#E53935",
+        });
+        const ring = new Three.Mesh(ringGeometry, ringMaterial);
 
-    gsap.to(triangle.rotation, {
-        duration: 6,
-        y: Math.PI * 2,
-        repeat: -1,
-        ease: "none",
-    });
+        ring.position.set(30, 10, 0);
 
-    gsap.to(ring.rotation, {
-        duration: 10,
-        y: Math.PI * 2,
-        repeat: -1,
-        ease: "none",
-    });
+        return ring;
+    };
 
-    render();
+    const createTriangle = () => {
+        const triangleGeometry = new Three.TorusGeometry(8, 1, 16, 3);
+        const triangleMaterial = new Three.MeshStandardMaterial({
+            color: "#4CAF50",
+        });
+        const triangle = new Three.Mesh(triangleGeometry, triangleMaterial);
 
-    function render() {
+        triangle.position.set(-20, 20, 0);
+
+        return triangle;
+    };
+
+    const createCube = () => {
+        const geometry = new Three.BoxGeometry(16, 16, 1);
+        const material = new Three.MeshStandardMaterial({ color: "#0e0398" });
+        const cube = new Three.Mesh(geometry, material);
+
+        cube.position.set(20, -20, 0);
+
+        return cube;
+    };
+
+    const createLight = () => {
+        const ambientLight = new Three.AmbientLight(0xffffff, 0.8);
+
+        ambientLight.position.set(-5, -5, -5);
+
+        return ambientLight;
+    };
+
+    const create = () => {
+        const light = createLight();
+        const cube = createCube();
+        const triangle = createTriangle();
+        const ring = createRing();
+
+        scene.add(light, cube, triangle, ring);
+
+        return { light, cube, triangle, ring };
+    };
+
+    const addAnimation = (obj) => {
+        const { triangle, ring } = obj;
+
+        gsap.to(triangle.rotation, {
+            duration: 6,
+            y: Math.PI * 2,
+            repeat: -1,
+            ease: "none",
+        });
+
+        gsap.to(ring.rotation, {
+            duration: 10,
+            y: Math.PI * 2,
+            repeat: -1,
+            ease: "none",
+        });
+    };
+
+    function draw() {
         renderer.render(scene, camera);
 
         controls.update();
 
-        requestAnimationFrame(render);
+        requestAnimationFrame(draw);
     }
 
-    window.addEventListener("resize", () =>
-        handleResize(renderer, camera, scene, controls)
-    );
+    const resize = () => {
+        handleResize(renderer, camera);
+    };
+
+    const addEvent = () => {
+        window.addEventListener("resize", () => handleResize(renderer, camera));
+    };
+
+    const initialize = () => {
+        const obj = create();
+        addAnimation(obj);
+        addEvent();
+        resize();
+        draw();
+    };
+
+    initialize();
 }
